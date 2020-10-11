1e4629914f75b25d4ac0dd5b28e10ddbb9df3047
S.X.Zhang <33718991+GXYM@users.noreply.github.com>
2020-10-11 14:10:20 +0800

    Update train_CTW1500.sh

--- tool/train_CTW1500.sh
+++ tool/train_CTW1500.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ../
-CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name Ctw1500 --max_epoch 600 --batch_size 6 --gpu 0 --input_size 640 --optim SGD --lr 0.001 --start_epoch 0 --viz --net vgg --resume pretrained/mlt2017_pretain/textsnake_vgg_100.pth
+CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name Ctw1500 --max_epoch 600 --batch_size 6 --gpu 0 --input_size 640 --optim SGD --lr 0.001 --start_epoch 0 --viz --net vgg 
+#--resume pretrained/mlt2017_pretain/textgraph_vgg_100.pth
