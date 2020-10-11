6640e2612ba3ab1413af32e9d8b81d3f22d3e5fe
S.X.Zhang <33718991+GXYM@users.noreply.github.com>
2020-10-11 14:11:13 +0800

    Update train_Icdar2015.sh

--- tool/train_Icdar2015.sh
+++ tool/train_Icdar2015.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ../
 CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name Icdar2015 --max_epoch 300 --batch_size 4 --gpu 0 --input_size 800 --optim SGD --lr 0.001 --start_epoch 1131 --num_workers 12 --viz --net vgg 
---resume pretrained/icdar15_pretain/textgraph_vgg_1130.pth 
+#--resume pretrained/icdar15_pretain/textgraph_vgg_1130.pth 
