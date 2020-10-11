ab46fb24f951f10fe6bde6ad32628e0d33e878fe
S.X.Zhang <33718991+GXYM@users.noreply.github.com>
2020-10-11 14:13:40 +0800

    Update train_TD500.sh

--- tool/train_TD500.sh
+++ tool/train_TD500.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ../
-CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name TD500 --max_epoch 2000 --batch_size 4 --gpu 1 --input_size 640 --optim SGD --lr 0.01 --start_epoch 0 --viz --net vgg --resume pretrained/mlt2017_pretain/textsnake_vgg_200.pth --start_epoch 0 --save_freq 50
+CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name TD500 --max_epoch 2000 --batch_size 4 --gpu 1 --input_size 640 --optim SGD --lr 0.01 --start_epoch 0 --viz --net vgg --start_epoch 0 --save_freq 50 
+#--resume pretrained/mlt2017_pretain/textgraph_vgg_200.pth 
