31c729074ab7d6f5933133e1fb990ff9daa34972
S.X.Zhang <33718991+GXYM@users.noreply.github.com>
2020-10-11 14:14:29 +0800

    Update train_Totaltxt.sh

--- tool/train_Totaltxt.sh
+++ tool/train_Totaltxt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd ../
-CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name Totaltext --max_epoch 600 --batch_size 4 --gpu 0 --input_size 800 --optim SGD --lr 0.001 --viz --num_workers 12 --net vgg --resume pretrained/totaltext_pretain/textsnake_vgg_300.pth --start_epoch 300
+CUDA_LAUNCH_BLOCKING=1 python train_TextGraph.py --exp_name Totaltext --max_epoch 600 --batch_size 4 --gpu 0 --input_size 800 --optim SGD --lr 0.001 --viz --num_workers 12 --net vgg 
+#--resume pretrained/totaltext_pretain/textgraph_vgg_300.pth --start_epoch 300
